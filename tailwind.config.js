6a41ece418828e38e2352751978ed8209ccbbfe1
RfxNaushad <rfxhridoy@gmail.com>
2024-09-15 10:29:06 +0600

    feat: folder structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '35': '35rem',
+    },
   },
   plugins: [],
+}
 }
