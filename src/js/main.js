5a1eeec045cb1d028b2d2c9c4897c1158c593a5d
Sourav Basak <souravbsk01@gmail.com>
2024-09-18 12:19:17 +0600

    business page complete

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,3 @@ function showAlert() {
     alert("Button clicked!");
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    const menuIcon = document.getElementById("menu-icon");
-    const menu = document.getElementById("menu");
-
-    menuIcon.addEventListener("click", function () {
-      if (menu.classList.contains("hidden")) {
-        menu.classList.remove("hidden");
-      } else {
-        menu.classList.add("hidden");
-      }
-    });
-  });
