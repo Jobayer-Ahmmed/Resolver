cbeb0c1b223574e6e3336134d7c21ef05b56137e
storngfornt <au0038173@gmail.com>
2024-09-19 18:08:42 +0600

    added popup logic in linkup js file

--- src/js/linkup.js
+++ src/js/linkup.js
@@ -5,3 +5,17 @@ document.getElementById('navSelect').addEventListener('change', function() {
         window.location.href = selectedValue;
     }
 });
+
+const fabBtn = document.getElementById('fab-btn');
+const popupMenu = document.getElementById('popup-menu');
+const closePopup = document.getElementById('close-popup');
+
+// Toggle the popup menu visibility on the floating action button click
+fabBtn.addEventListener('click', () => {
+  popupMenu.classList.toggle('hidden');
+});
+
+// Hide the popup when the close button is clicked
+closePopup.addEventListener('click', () => {
+  popupMenu.classList.add('hidden');
+});
